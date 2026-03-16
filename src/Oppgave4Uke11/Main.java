f703016b43319d1c5114a75835c7c6936a2158cf
cirashrii <cirashrii@gmail.com>
2026-03-16 10:52:54 +0100

    la inn eksempelkoden men kommentert

--- src/Oppgave4Uke11/Main.java
+++ src/Oppgave4Uke11/Main.java
@@ -3,5 +3,12 @@ package Oppgave4Uke11;
 public class Main {
     public static void main(String[] args) {
 
+
+//        int tall = 376; // Her kan vi bruke eit vilkårleg tal
+//        for (int i = 0; i < antElement){
+//            // legg tall til i HashSet og tabell
+//            tall = (tall + 45713) % 1000000; // Sjå nedanfor om 45713
+//        }
+
     }
 }
